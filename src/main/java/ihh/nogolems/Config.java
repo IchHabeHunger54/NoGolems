2161c254b20594b3c9e4083861ce27446d2aabaf
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-09-28 20:19:06 +0200

    update to 1.21.1

--- src/main/java/ihh/nogolems/Config.java
+++ src/main/java/ihh/nogolems/Config.java
@@ -1,15 +1,15 @@
 package ihh.nogolems;
 
-import net.minecraftforge.common.ForgeConfigSpec;
+import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class Config {
-    public static ForgeConfigSpec SPEC;
-    public static ForgeConfigSpec.BooleanValue DISABLE_IRON_GOLEM;
-    public static ForgeConfigSpec.BooleanValue DISABLE_SNOW_GOLEM;
-    public static ForgeConfigSpec.BooleanValue DISABLE_WITHER;
+    public static ModConfigSpec SPEC;
+    public static ModConfigSpec.BooleanValue DISABLE_IRON_GOLEM;
+    public static ModConfigSpec.BooleanValue DISABLE_SNOW_GOLEM;
+    public static ModConfigSpec.BooleanValue DISABLE_WITHER;
 
     static {
-        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
         DISABLE_IRON_GOLEM = builder.comment("Disables creation of iron golems.").define("disable_iron_golem", true);
         DISABLE_SNOW_GOLEM = builder.comment("Disables creation of snow golems.").define("disable_snow_golem", true);
         DISABLE_WITHER = builder.comment("Disables creation of withers.").define("disable_wither", true);
--- src/main/java/ihh/nogolems/NoGolems.java
+++ src/main/java/ihh/nogolems/NoGolems.java
@@ -1,14 +1,17 @@
 package ihh.nogolems;
 
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(NoGolems.MOD_ID)
 public class NoGolems {
     public static final String MOD_ID = "nogolems";
 
-    public NoGolems() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+    public NoGolems(ModContainer container) {
+        container.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 }
